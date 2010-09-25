019d494a1892e3937bc9db25687ab2ff8e081951
rwinston <rory@theresearchkitchen.com>
2010-09-25 12:40:07 +1000

    Fix up test

--- src/test/java/org/netling/ssh/util/Base64Test.java
+++ src/test/java/org/netling/ssh/util/Base64Test.java
@@ -1,5 +1,7 @@
 package org.netling.ssh.util;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.IOException;
 
 import org.junit.Test;
@@ -12,9 +14,8 @@ public class Base64Test {
 	public void testEncodeDecode() throws IOException {
 		String s = "Hello world";
 		String encoded = Base64.encodeBytes(s.getBytes());
-		System.out.println(encoded);
 		String decoded = new String(Base64.decode(encoded));
-		System.out.println(decoded);
+		assertEquals(s, decoded);
 	}
 
 }
